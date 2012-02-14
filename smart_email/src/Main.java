fe566f1296ec7272e3991e92a479302570ba4128
Moustafa Meshry <lifemaker.fi@gmail.com>
2012-02-15 00:20:02 +0200

    main method for testing

--- smart_email/src/Main.java
+++ smart_email/src/Main.java
@@ -1,7 +1,13 @@
+import classification.ClassificationManager;
+import classification.Classifier;
+
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		ClassificationManager mgr = new ClassificationManager();
+		String path = "enron_processed/lokay_m";
+		Classifier cls = mgr.go("FileSystem", path, null);
 		System.out.println("Hello World!");
 	}
 }
