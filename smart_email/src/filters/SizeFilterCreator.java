e4d18944a9cc3973765059bdffa7d228bda61ad9
Amr Sharaf <amr.nabil.sharaf@gmail.com>
2012-02-24 21:52:45 +0200

    added a new filter -- email size filter

---
+++ smart_email/src/filters/SizeFilterCreator.java
@@ -0,0 +1,28 @@
+package filters;
+
+import java.util.ArrayList;
+
+import weka.core.Attribute;
+import general.Email;
+
+/**
+ * 
+ * @author Amr Sharaf
+ *
+ */
+public class SizeFilterCreator implements FilterCreator {
+
+	// Attribute name for the email size feature.
+	private static final String SIZE_ATTRIBUTE = "SizeAttribute";
+	
+	@Override
+	public Filter createFilter(Email[] emails) {
+		// Prepare for adding a new attribute for the email size.
+		ArrayList<Attribute> attributes = new ArrayList<Attribute>(1);
+		attributes.add(new Attribute(SIZE_ATTRIBUTE));
+		// Return a new filter after adding the attribute type and given
+		// no options.
+		return new SizeFilter(attributes, null); 
+	}
+
+}
