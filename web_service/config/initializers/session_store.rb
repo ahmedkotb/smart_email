4490a28296993fd70237e7d204c427609a3e1c69
Ahmed El.Sharkasy <ahmed.elsharkasy@espace.com.eg>
2012-03-31 21:43:40 +0200

    Created User and Account model/view/controller and their relation

---
+++ web_service/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SmartEmail::Application.config.session_store :cookie_store, key: '_smart_email_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SmartEmail::Application.config.session_store :active_record_store
