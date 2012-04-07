c28d2f42ddbfe1bbbebf635bb13b209b70732cbf
Ahmed Kotb <kotbcorp@gmail.com>
2012-04-07 14:48:58 +0200

    Fixed 2 bugs on WordsCleaner.java (preprocessor)
    1- split("\W+") didn't split on underscores so it must be handled manually to avoid having underscores in the body or the subject of emails
    2- split method can return empty words at the beginning of the array .. so those must be removed

--- smart_email/src/preprocessors/WordsCleaner.java
+++ smart_email/src/preprocessors/WordsCleaner.java
@@ -3,8 +3,8 @@ package preprocessors;
 import general.Email;
 
 /**
- * removes punctuations and any non alphanumeric characters from an email and
- * converts the email into words separated with single white space
+ * removes punctuation and any non alphanumeric characters from an email
+ * converting it into words separated with single white space
  * 
  * @author ahmedkotb
  * 
@@ -15,15 +15,22 @@ public class WordsCleaner implements Preprocessor {
 
 	@Override
 	public void apply(Email email) {
-		// TODO Abbas please review
 		try {
 			// splits the email on any non alpha numeric characters
+
+			//two special cases has to be handled
+			//1- split method can return an empty space at the
+			//   Beginning of the array
+			//2- \W doesn't split on underscores
 
 			// process the subject
 			String[] words = email.getSubject().split(splitRegex);
 			String subject = "";
-			for (String word : words)
+			for (String word : words){
+				if (word.equals("") || word.equals("_"))
+					continue;
 				subject += word + " ";
+			}
 			email.setSubject(subject);
 
 			// process the content
@@ -31,12 +38,16 @@ public class WordsCleaner implements Preprocessor {
 			StringBuilder sb = new StringBuilder(
 					((String) email.getContent()).length());
 
-			for (String word : words)
+			for (String word : words){
+				if (word.equals("") || word.equals("_"))
+					continue;
 				sb.append(word + " ");
+			}
 
 			email.setContent(sb.toString(), "text/plain");
 		} catch (Exception ex) {
-			//ignore
+			ex.printStackTrace();
+			System.exit(1);
 		}
 	}
 
