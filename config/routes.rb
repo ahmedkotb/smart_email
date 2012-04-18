a4f21376b6629f2aad25b79bef97866370f319c5
Ahmed El.Sharkasy <ahmed.elsharkasy@espace.com.eg>
2012-04-18 20:51:07 +0200

    adding a break condition to the routes file to solve assets problem in heruko

--- config/routes.rb
+++ config/routes.rb
@@ -65,4 +65,5 @@ SmartEmail::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+break if ARGV.join.include? 'assets:precompile'
 end
