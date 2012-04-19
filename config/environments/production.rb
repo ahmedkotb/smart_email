07263b313428f616470f640961abdc27bd292787
Ahmed El.Sharkasy <ahmed.elsharkasy@espace.com.eg>
2012-04-19 20:56:30 +0200

    trying to solve heruko devise gem error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ SmartEmail::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-	config.action_mailer.default_url_options = { :host => 'smart-email.heroku.com' }
+	config.action_mailer.default_url_options = { :host => 'smart-email.herokuapp.com' }
 end
