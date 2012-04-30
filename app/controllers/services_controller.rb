aae6e7d85b8065bcf9f5fc148cb075ed32a74386
Ahmed El.Sharkasy <ahmed.elsharkasy@espace.com.eg>
2012-05-01 01:52:42 +0200

    debugging services controller code

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,7 +3,10 @@ class ServicesController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider=>auth["provider"],:uid=> auth["uid"]).first|| User.create_with_omniauth(auth)
+    user = User.where(:provider=>auth["provider"],:uid=> auth["uid"]).first
+		if user.nil?
+			User.create_with_omniauth(auth)
+		end
     service[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
   end
