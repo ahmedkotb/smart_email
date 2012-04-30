40ff84306e3992a9061bfbbdb2c6cee3023976fe
Ahmed El.Sharkasy <ahmed.elsharkasy@espace.com.eg>
2012-05-01 01:47:05 +0200

    find=>Where

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,7 +3,7 @@ class ServicesController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.where(:provider=>auth["provider"],:uid=> auth["uid"]).first|| User.create_with_omniauth(auth)
     service[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
   end
